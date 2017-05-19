f3219455043538746512ced463396639895f07da
3ntropia <fmartinhuber@gmail.com>
2017-05-19 11:54:17 -0300

    Prueba2?

--- HeyCore/src/main/java/com/heysteve/heygym/heycore/HeyGymMain.java
+++ HeyCore/src/main/java/com/heysteve/heygym/heycore/HeyGymMain.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//aasdasd
+//prueba user/email
 public class HeyGymMain {
 
 
